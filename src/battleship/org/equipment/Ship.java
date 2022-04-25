767eb46b759b07dece9e589bee132c9ee2158a46
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-04-25 20:27:50 +0300

    fixed display of enemy field

--- src/battleship/org/equipment/Ship.java
+++ src/battleship/org/equipment/Ship.java
@@ -1,11 +1,11 @@
 package battleship.org.equipment;
 
 public enum Ship {
-    AIRCRAFT_CARRIER(5, "Aircraft Carrier")/*,
+    AIRCRAFT_CARRIER(5, "Aircraft Carrier"),
    BATTLESHIP(4, "Battleship"),
     SUBMARINE(3, "Submarine"),
     CRUISER(3, "Cruiser"),
-    DESTROYER(2, "Destroyer")*/;
+    DESTROYER(2, "Destroyer");
 
     final int sell;
     final String type;
